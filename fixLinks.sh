39b24d1be3c269ec68f68f42baca9582cb3d5aae
James <s3656070@student.rmit.edu.au>
2020-09-06 17:44:57 +1000

    sda

--- fixLinks.sh
+++ fixLinks.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rootURL="https://jameswrc.github.io/bonfire.github.io/"
+rootURL="bonfire.github.io/"
 keyWord="assets/"
 dir="/Users/james/Documents/GitHub/Sites/bonfire.github.io/"
 sed -i -e 's,'"${keyWord}"','"${rootURL}${keyWord}"',g' "${dir}index.html"
