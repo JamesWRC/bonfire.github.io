17e29a60578b00575b9caa43cfc37c830448cf61
James <s3656070@student.rmit.edu.au>
2020-09-06 17:39:42 +1000

    fixed links

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
+async function handleRequest() {
+  const init = {
+    headers: {
+      'content-type': 'text/html;charset=UTF-8',
+    },
+  }
+  const response = await fetch(url, init)
+  const results = await gatherResponse(response)
+  return new Response(results, init)
+}
 addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
+  return event.respondWith(handleRequest())
 })
 /**
- * Respond with hello worker text
- * @param {Request} request
+ * gatherResponse awaits and returns a response body as a string.
+ * Use await gatherResponse(..) in an async function to get the response body
+ * @param {Response} response
  */
-async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+async function gatherResponse(response) {
+  const { headers } = response
+  const contentType = headers.get('content-type') || ''
+  if (contentType.includes('application/json')) {
+    return JSON.stringify(await response.json())
+  } else if (contentType.includes('application/text')) {
+    return await response.text()
+  } else if (contentType.includes('text/html')) {
+    return await response.text()
+  } else {
+    return await response.text()
+  }
 }
+/**
+ * Example someHost at url is set up to respond with HTML
+ * Replace url with the host you wish to send requests to
+ */
+const url = 'https://jameswrc.github.io/bonfire.github.io/'
